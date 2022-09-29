ff1313e3ef3cc64c36d629833f2a9a51cf258f32
onionsalot <onionsinmypants@gmail.com>
2022-09-29 00:53:40 -0400

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  get 'static/home'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  resources :sessions, only: [:create]
+  resources :registrations, only: [create]
+  delete :logout, to: "sessions#logout"
+  get :logged_in, to: "sessions#logged_in"
+  root to: "static#home"
+end
